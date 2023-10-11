7b6292f2cbb42b3bf60ee6299919c4d06ca5555c
Alena Astrakhantseva <alena@dlthub.com>
2023-10-11 12:52:56 +0000

    move project.ysml to dbt folder

--- dbt_pokemon/pokemon/models/example/my_second_dbt_model.sql
+++ dbt_pokemon/pokemon/models/example/my_second_dbt_model.sql
@@ -1,6 +1,6 @@
 -- Use the `ref` function to select from other models
 {{ config(materialized="table") }}
 
-with two_pokemon as (select name from `dlt-dev-external.pokemon_data.pokemon` limit 2)
+with two_pokemon as (select name from {{ ref(my_first_dbt_model) }} limit 2)
 select *
 from two_pokemon
